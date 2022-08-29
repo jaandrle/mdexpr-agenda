ebe487ff783dd191a66b75596d3f5e04b9629c55
Jan Andrle <andrle.jan@centrum.cz>
2022-08-29 15:52:26 +0200

    :boom:

--- bin/mdexpr-agenda.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-/* global require:true */
-require = require('esm')(module /*, options*/);
-const { mdexpr }= require('../src/mdexpr');
-
-mdexpr({
-	oncli: console.log
-});
---
+++ bin/mdexpr-agenda.mjs
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import { mdexpr } from "../src/mdexpr.js";
+
+mdexpr({
+	oncli: console.log
+});
